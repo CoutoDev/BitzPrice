b05c616a4f4f46fcfc739aa5c2ea1e2158bb32df
Vitor Coutinho <vitor.coutinho@rcaturismo.com.br>
2020-04-24 17:10:39 -0300

    Commit inicial

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About My Next Js APP</h1>
+            <p>Application to view BTC Prices</p>
+        </div>
+    </Layout>
+);
+
+export default About;
